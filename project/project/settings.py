e847cde58cb9518785b9352494931fbf7b31294c
Dogeon0 <76671616+Dogeon0@users.noreply.github.com>
2024-05-23 16:31:18 -0300

    staticfiles copied to folder

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-$u2=q4r3*z7p#yfmht&=e(vbzpld419y2s1e=t%&)og2rjtc^r'
 SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','dogeon.pythonanywhere.com']
 
 
 # Application definition
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
